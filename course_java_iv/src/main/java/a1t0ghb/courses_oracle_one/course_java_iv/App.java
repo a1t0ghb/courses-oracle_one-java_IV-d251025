b19524c1909fcae30c68809801bbf66caad02d24
a1t0git <github_ctc101@a1t0.aleeas.com>
2025-10-25 10:20:21 -0500

    chore: init. blank Java project with 'Maven'.

---
+++ course_java_iv/src/main/java/a1t0ghb/courses_oracle_one/course_java_iv/App.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+//  Package location definition.
+package a1t0ghb.courses_oracle_one.course_java_iv;
+
+//  IMPORTS: external classes and interfaces.
+
+
+/**
+ *
+ * @author a1t0ghb
+ */
+public class App {
+
+    public static void main(String[] args) {
+        // System.out.println("Hello World!");
+        
+        //  INITIALIZATION OF INSTANCES.
+
+    }
+}
+//  EOF.
