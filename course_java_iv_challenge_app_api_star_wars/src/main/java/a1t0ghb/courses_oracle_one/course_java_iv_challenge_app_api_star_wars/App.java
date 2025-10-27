1394a24ca67311bb7ffb49593cb8e175bcd01678
a1t0git <github_ctc101@a1t0.aleeas.com>
2025-10-27 20:01:48 +0000

    chore: init. blank Java project with 'Maven'.

---
+++ course_java_iv_challenge_app_api_star_wars/src/main/java/a1t0ghb/courses_oracle_one/course_java_iv_challenge_app_api_star_wars/App.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+//  Package location definition.
+package a1t0ghb.courses_oracle_one.course_java_iv_challenge_app_api_star_wars;
+
+//  IMPORTS: external classes and interfaces.
+
+//  IMPORTS - UTILITIES.
+//  Java utilities:
+//  - Shortcut for importing ALL Java Utils: 'import java.util.*;'.
+
+//  IMPORTS - CUSTOM CLASSES.
+
+/**
+ *
+ * @author a1t0ghb
+ */
+public class App {
+
+    public static void main(String[] args) {
+        // System.out.println("Hello World!");
+
+        //  INITIALIZATION OF INSTANCES.
+
+    }
+}
+//  EOF.
