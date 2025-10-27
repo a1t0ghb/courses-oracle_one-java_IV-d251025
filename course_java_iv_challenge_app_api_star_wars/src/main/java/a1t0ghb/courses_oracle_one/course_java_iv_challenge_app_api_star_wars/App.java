084e7ac1d36a11c87d612b98f02cf2317bb0f093
a1t0git <github_ctc101@a1t0.aleeas.com>
2025-10-27 22:29:49 +0000

    feat: add input from user for film number.

--- course_java_iv_challenge_app_api_star_wars/src/main/java/a1t0ghb/courses_oracle_one/course_java_iv_challenge_app_api_star_wars/App.java
+++ course_java_iv_challenge_app_api_star_wars/src/main/java/a1t0ghb/courses_oracle_one/course_java_iv_challenge_app_api_star_wars/App.java
@@ -10,6 +10,7 @@ package a1t0ghb.courses_oracle_one.course_java_iv_challenge_app_api_star_wars;
 //  IMPORTS - UTILITIES.
 //  Java utilities:
 //  - Shortcut for importing ALL Java Utils: 'import java.util.*;'.
+import java.util.Scanner;                       //  User input via console / terminal.
 
 //  IMPORTS - CUSTOM CLASSES.
 import a1t0ghb.courses_oracle_one.course_java_iv_challenge_app_api_star_wars.models.ConsultaPelicula;
@@ -25,14 +26,37 @@ public class App {
         // System.out.println("Hello World!");
 
         //  INITIALIZATION OF INSTANCES.
-        ConsultaPelicula consulta = new ConsultaPelicula();
+        //  'Scanner' as class to receive input of user from console.
+        //  - Use in conjunction with try-with-resources, to AUTOMATICALLY close resources and manage try-cath exceptions. Ref: 'https://www.w3schools.com/java/java_try_catch_resources.asp'.
+        try (Scanner lectura = new Scanner(System.in)) {
 
-        //  Pelicula existente.
-        Pelicula pelicula = consulta.buscaPelicula(1);
-        System.out.println(pelicula);
-        //  Pelicula in-existente.
-        Pelicula pelicula2 = consulta.buscaPelicula(9);
-        System.out.println(pelicula2);
+            ConsultaPelicula consulta = new ConsultaPelicula();
+            System.out.println("Escriba número de la película: ");
+
+            // //  Pelicula existente.
+            // Pelicula pelicula1 = consulta.buscaPelicula(1);
+            // System.out.println(pelicula1);
+            // //  Pelicula in-existente.
+            // Pelicula pelicula2 = consulta.buscaPelicula(9);
+            // System.out.println(pelicula2);
+
+            try {
+
+                var numeroDePelicula = Integer.valueOf(lectura.nextLine());
+                Pelicula pelicula = consulta.buscaPelicula(numeroDePelicula);
+                System.out.println(pelicula);
+
+            } catch (NumberFormatException e) {
+
+                System.out.println("Número no encontrado: " + e.getMessage());
+
+            } catch (RuntimeException e) {
+
+                System.out.println(e.getMessage());
+                System.out.println("Finalizó ejecución del programa.\n");
+            }
+        
+        }
 
     }
     
