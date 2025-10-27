34d162391da2e07ae4ef64d485e5f29002f44400
a1t0git <github_ctc101@a1t0.aleeas.com>
2025-10-27 22:29:13 +0000

    refactor: re-ordered code and adjust 'try-catch' in 'ConsultaPelicula'.

--- course_java_iv_challenge_app_api_star_wars/src/main/java/a1t0ghb/courses_oracle_one/course_java_iv_challenge_app_api_star_wars/models/ConsultaPelicula.java
+++ course_java_iv_challenge_app_api_star_wars/src/main/java/a1t0ghb/courses_oracle_one/course_java_iv_challenge_app_api_star_wars/models/ConsultaPelicula.java
@@ -17,7 +17,7 @@ import java.net.http.HttpClient;                //  HTTP requests.
 import java.net.http.HttpRequest;               //  HTTP requests.
 import java.net.URI;                            //  HTTP requests.
 import java.net.http.HttpResponse;              //  HTTP requests.
-import java.io.IOException;                     //  To handle IO exceptions (i.e. possible errors); e.g. when making an HTTP request, from method '.send()'.
+// import java.io.IOException;                     //  To handle IO exceptions (i.e. possible errors); e.g. when making an HTTP request, from method '.send()'.
 
 //  JSON:
 //  - 'Gson': 'https://github.com/google/gson', 'https://www.javadoc.io/doc/com.google.code.gson/gson/latest/com.google.gson/module-summary.html'.
@@ -31,31 +31,31 @@ import com.google.gson.Gson;                    //  JSON transformations.
  */
 public class ConsultaPelicula {
 
-    @SuppressWarnings("unused")
+    @SuppressWarnings({"unused", "UseSpecificCatch"})
     public Pelicula buscaPelicula(int numeroDePelicula) {
 
         URI direccion = URI.create(
-            "https://swapi.dev/api/films/" + numeroDePelicula);     //  Uses an instance of class 'URI'. Requires IMPORT package 'java.net.URI'.
+            "https://swapi.dev/api/films/" + numeroDePelicula);         //  Uses an instance of class 'URI'. Requires IMPORT package 'java.net.URI'.
 
         //  HTTP request.
-        HttpClient client = HttpClient.newHttpClient();             //  Creates http client. Requires IMPORT package 'java.net.http.HttpClient'.
-        HttpRequest request = HttpRequest.newBuilder()              //  Creates http request, using pattern BUILDER. 'HttpRequest' is NOT a class, more likey an interface, which means an instance of it CAN'T be created as a regular class. Requires IMPORT class 'java.net.http.HttpRequest'.
+        HttpClient client = HttpClient.newHttpClient();                 //  Creates http client. Requires IMPORT package 'java.net.http.HttpClient'.
+        HttpRequest request = HttpRequest.newBuilder()                  //  Creates http request, using pattern BUILDER. 'HttpRequest' is NOT a class, more likey an interface, which means an instance of it CAN'T be created as a regular class. Requires IMPORT class 'java.net.http.HttpRequest'.
             .uri(direccion)
-            .build();                                               //  Instruction from pattern BUILDER. Way to recognize it.
-        
-        HttpResponse<String> response = null;                        //  Creates http response. Requires IMPORT package 'java.net.http.HttpResponse'.
-        
+            .build();                                                   //  Instruction from pattern BUILDER. Way to recognize it.
+                
         try {
 
-            response = client
-            .send(request, HttpResponse.BodyHandlers.ofString());   //  Method '.send()' requires handle EXCEPTIONS. For now, will be added to 'public static void main(String[] args) { ... }'.
+            HttpResponse<String> response = client                      //  Creates http response. Requires IMPORT package 'java.net.http.HttpResponse'.
+                .send(request, HttpResponse.BodyHandlers.ofString());   //  Method '.send()' requires handle EXCEPTIONS. For now, will be added to 'public static void main(String[] args) { ... }'.
 
-        } catch (IOException | InterruptedException e) {            //  Multi-catch, for exceptions.
+            return new Gson().fromJson(response.body(), Pelicula.class);//  Returns a new instance of 'Pelicula', using HTTP request response and transforming JSON with Gson.
 
-            throw new RuntimeException(e);
+        // } catch (IOException | InterruptedException e) {            //  Multi-catch, for exceptions.
+        } catch (Exception e) {                                         //  General exception catch.
+
+            throw new RuntimeException("Error; no encontré esa película: " + e);
             
         }
         
-        return new Gson().fromJson(response.body(), Pelicula.class);//  Returns a new instance of 'Pelicula', using HTTP request response and transforming JSON with Gson.
     }
 }
